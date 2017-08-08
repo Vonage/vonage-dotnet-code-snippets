7718b671404cf353178f1f4d1ce77973f3b8dd52
DESKTOP-8V16LNK\othma <rabeb.othmani@vonage.com>
2017-08-08 12:32:54 +0100

    new helper to create Stream NCCO

--- NexmoVoiceASPNetCoreQuickStarts/Helpers/NCCOHelpers.cs
+++ NexmoVoiceASPNetCoreQuickStarts/Helpers/NCCOHelpers.cs
@@ -1,20 +1,44 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Microsoft.AspNetCore.Hosting.Internal;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace NexmoVoiceASPNetCoreQuickStarts.Helpers
 {
     public class NCCOHelpers
     {
-        public void CreateTalkNCCO()
+        public void CreateTalkNCCO(string rootpath, string callText, string callVoice)
         {
             dynamic TalkNCCO = new JObject();
-            TalkNCCO.action = "Talk";
-            TalkNCCO.text = "You are listening to a text-to-speech Call made with Nexmo Voice API";
-            TalkNCCO.voiceName = "Amy";
-            var test = TalkNCCO.ToString();
+            TalkNCCO.action = "talk";
+            TalkNCCO.text = callText;
+            TalkNCCO.voiceName = callVoice;
+
+            var pathToFile = Path.Combine(rootpath,"TalkNCCO.json");
+            using (StreamWriter s = File.CreateText(pathToFile))
+            {
+              s.Write(TalkNCCO.ToString());
+            }
+        }
+
+        public void CreateStreamNCCO(string rootpath, string[] streamUrl, int level, bool bargeIn, int loopTimes)
+        {
+            dynamic StreamNCCO = new JObject();
+            StreamNCCO.action = "stream";
+            StreamNCCO.streamUrl = streamUrl;
+            StreamNCCO.level = level;
+            StreamNCCO.bargeIn = bargeIn;
+            StreamNCCO.loop = loopTimes;
+
+            var pathToFile = Path.Combine(rootpath, "StreamNCCO.json");
+            using (StreamWriter s = File.CreateText(pathToFile))
+            {
+                s.Write(StreamNCCO.ToString());
+            }
         }
     }
 }
