647b693b2fbcff0e53ef566ff1f95c868ddc16fd
Rabeb Othmani <rabeb.othmani@vonage.com>
2018-11-21 08:48:03 +0000

    Added Inbound call building block using NancyFX

--- NexmoVoiceASPNetCoreQuickStarts/Startup.cs
+++ NexmoVoiceASPNetCoreQuickStarts/Startup.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
+using Nancy.Owin;
 using System.IO;
-using Microsoft.AspNetCore.Http;
 
 namespace NexmoVoiceASPNetCoreQuickStarts
 {
@@ -65,6 +62,11 @@ namespace NexmoVoiceASPNetCoreQuickStarts
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            app.UseOwin(x => x.UseNancy());
+
         }
+
+        
     }
 }
---
+++ NexmoVoiceASPNetCoreQuickStarts/VoiceModule.cs
@@ -0,0 +1,28 @@
+﻿using Nancy;
+using Newtonsoft.Json.Linq;
+
+namespace NexmoVoiceASPNetCoreQuickStarts
+{
+    public class VoiceModule : NancyModule 
+    {
+        public VoiceModule()
+        {
+            Get["/webhook/answer"] = x => GetInboundNCCO();
+            Post["/webhook/event"] = x => this.Request.Query["status"];
+        }
+
+        private string GetInboundNCCO()
+        {
+            dynamic TalkNCCO = new JObject();
+            TalkNCCO.action = "talk";
+            TalkNCCO.text = "Thank you for calling from "+ string.Join(" ", this.Request.Query["from"].ToCharArray());
+            TalkNCCO.voiceName = "Kimberly";
+            
+            JArray jarrayObj = new JArray();
+            jarrayObj.Add(TalkNCCO);
+
+            return jarrayObj.ToString();
+
+        }
+    }
+}
