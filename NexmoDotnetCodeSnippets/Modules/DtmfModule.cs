c73fafc0fda186e93504da724d57d7a4adff155f
Rabeb Othmani <rabeb.othmani@vonage.com>
2019-07-21 12:24:59 +0100

    [WIP]: Code snippets- added Voice modules

---
+++ NexmoDotnetCodeSnippets/Modules/DtmfModule.cs
@@ -0,0 +1,64 @@
+﻿using Nancy;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NexmoDotnetCodeSnippets.Modules
+{
+    public class DtmfModule : NancyModule
+    {
+        public DtmfModule()
+        {
+            Get("/webhook/answer", x => { var response = (Response)GetDTMFNCCO();
+                                          response.ContentType = "application/json";
+                                          return response;
+            });
+
+            Get("/webhook/dtmf", x => { var response = (Response)GetDTMFInput();
+                                        response.ContentType = "application/json";
+                                        return response;
+            });
+
+            Post("webhook/event", x => Request.Query["status"]);
+        }
+
+        private string GetDTMFNCCO()
+        {
+            dynamic TalkNCCO = new JObject();
+            TalkNCCO.action = "talk";
+            TalkNCCO.text = "Hello. Please press any key to continue.";
+
+            JArray jarrayObj = new JArray
+            {
+                TalkNCCO
+            };
+
+            dynamic InputNCCO = new JObject();
+            InputNCCO.action = "input";
+            InputNCCO.maxDigits = "1";
+            InputNCCO.eventUrl = $"{Request.Url.SiteBase}/webhook/dtmf";
+
+            jarrayObj.Add(InputNCCO);
+
+            return jarrayObj.ToString();
+
+        }
+
+        private string GetDTMFInput()
+        {
+            dynamic TalkNCCO = new JObject();
+            TalkNCCO.action = "talk";
+            TalkNCCO.text = $"You pressed {Request.Query["dtmf"]} ";
+
+            JArray jarrayObj = new JArray
+            {
+                TalkNCCO
+            };
+
+            return jarrayObj.ToString();
+
+        }
+    }
+}
---
+++ NexmoDotnetCodeSnippets/Modules/InboundCallModule.cs
@@ -0,0 +1,35 @@
+﻿using Nancy;
+using Newtonsoft.Json.Linq;
+
+namespace NexmoDotnetCodeSnippets.Modules
+{
+    public class InboundCallModule : NancyModule
+    {
+        public InboundCallModule()
+        {
+            Get("/webhook/answer", x => {
+                var response = (Response)GetInboundNCCO();
+                response.ContentType = "application/json";
+                return response;
+            });
+
+            Post("/webhook/event", x => Request.Query["status"]);
+        }
+
+        private string GetInboundNCCO()
+        {
+            dynamic TalkNCCO = new JObject();
+            TalkNCCO.action = "talk";
+            TalkNCCO.text = "Thank you for calling from " + string.Join(" ", this.Request.Query["from"].ToCharArray());
+            TalkNCCO.voiceName = "Kimberly";
+
+            JArray jarrayObj = new JArray
+            {
+                TalkNCCO
+            };
+
+            return jarrayObj.ToString();
+
+        }
+    }
+}
