ed32d9b296dcf28c3929a97c253a7929a4449bdd
Rabeb Othmani <rabeb.othmani@vonage.com>
2019-07-28 18:08:35 +0100

    [WIP] code snippets: added Search messages code samples

--- NexmoDotnetCodeSnippets/Controllers/SearchController.cs
+++ NexmoDotnetCodeSnippets/Controllers/SearchController.cs
@@ -3,13 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Nexmo.Api;
 
 namespace NexmoDotnetCodeSnippets.Controllers
 {
     public class SearchController : Controller
     {
+        public Client Client { get; set; }
+
+        public SearchController()
+        {
+            Client = new Client(creds: new Nexmo.Api.Request.Credentials
+            {
+                ApiKey = "NEXMO_API_KEY",
+                ApiSecret = "NEXMO_API_SECRET"
+            });
+        }
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SearchMessage(string messageID)
+        {
+            var message = Client.Search.GetMessage(messageID);
+
+            if (message.messageId != null)
+            {
+                ViewData.Add("message", message);
+            }
+            else
+            {
+                bool notFound = true;
+                ViewData.Add("notFound", notFound);
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SearchRejection(string messageID, string number, DateTime rejectionDate)
+        {
+            string date = rejectionDate.Year + "-" + rejectionDate.Month + "-" + rejectionDate.Day;
+            var msgs = Client.Search.GetRejections(new Search.SearchRequest
+            {
+                date = date,
+                to = number
+            });
+
+            if (msgs.items != null)
+            {
+                ViewData.Add("items", msgs.items);
+            }
+            else
+            {
+                bool notFound = true;
+                ViewData.Add("notFound", notFound);
+            }
+
             return View();
         }
     }
