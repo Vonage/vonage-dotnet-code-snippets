6a72e8922a8e554a7d46c6b567f8842624d396bc
slorellovidyo <slorello89@gmail.com>
2019-09-13 13:29:26 -0400

    aligning with specs

--- NexmoDotnetCodeSnippets/Senders/SMSSender.cs
+++ NexmoDotnetCodeSnippets/Senders/SMSSender.cs
@@ -1,40 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Nexmo.Api;
+﻿using Nexmo.Api;
 
 namespace NexmoDotnetCodeSnippets.Senders
 {
     public class SMSSender
-    {
-        private const string API_KEY = "NEXMO_API_KEY";
-        private const string API_SECRET = "NEXMO_API_SECRET";        
+    {   
 
-        public static SMS.SMSResponse SendSMS(string toNumber, string fromNumber, string message)
-        {     
+        public static SMS.SMSResponse SendSMS(string TO_NUMBER, string NEXMO_BRAND_NAME, string message)
+        {
+            const string API_KEY = "NEXMO_API_KEY";
+            const string API_SECRET = "NEXMO_API_SECRET";
             var client = new Client(creds: new Nexmo.Api.Request.Credentials(nexmoApiKey: API_KEY, nexmoApiSecret: API_SECRET));
 
             var results = client.SMS.Send(new SMS.SMSRequest
             {
-                from = fromNumber,
-                to = toNumber,
+                from = NEXMO_BRAND_NAME,
+                to = TO_NUMBER,
                 text = message
             });
 
             return results;
         }
 
-        public static SMS.SMSResponse SendSMSUnicode(string toNumber, string fromNumber, string message)
+        public static SMS.SMSResponse SendSMSUnicode(string TO_NUMBER, string NEXMO_BRAND_NAME, string message)
         {
-            var client = new Client(creds: new Nexmo.Api.Request.Credentials(nexmoApiKey: API_KEY, nexmoApiSecret: API_SECRET));
+            const string NEXMO_API_KEY = "NEXMO_API_KEY";
+            const string NEXMO_API_SECRET = "NEXMO_API_SECRET";
+            var client = new Client(creds: new Nexmo.Api.Request.Credentials(nexmoApiKey: NEXMO_API_KEY, nexmoApiSecret: NEXMO_API_SECRET));
 
             var messageType = "unicode";
             var results = client.SMS.Send(new SMS.SMSRequest
             {
-                from = fromNumber,
-                to = toNumber,
+                from = NEXMO_BRAND_NAME,
+                to = TO_NUMBER,
                 text = message,
                 type = messageType
             });
