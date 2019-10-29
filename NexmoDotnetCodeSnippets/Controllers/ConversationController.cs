03a17a16fcf34472d8bab34e5b1ca72f0dc61e44
slorello89 <slorello89@gmail.com>
2019-10-29 10:37:42 -0400

    inital Conversation fixes

---
+++ NexmoDotnetCodeSnippets/Controllers/ConversationController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NexmoDotnetCodeSnippets.Senders;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NexmoDotnetCodeSnippets.Controllers
+{
+    public class ConversationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateConversation(string name, string displayName, string imageUrl)
+        {
+            var response = ConversationSender.CreateConversation(name, displayName, imageUrl);
+            ViewBag.CreateDisplayName = response.DisplayName;
+            ViewBag.CreateId = response.Id;
+            ViewBag.CreateImageUrl = response.ImageUrl;
+            ViewBag.CreateSelf = response.Links.Self;
+            ViewBag.CreateName = response.Name;
+            ViewBag.CreateProperties = response.Properties;
+            ViewBag.CreateTimestamp = response.Timestamp;
+
+            return View("index");
+        }
+
+        public IActionResult GetConversation(string id)
+        {
+            var response = ConversationSender.GetConversation(id);
+            ViewBag.GetDisplayName = response.DisplayName;
+            ViewBag.GetId = response.Id;
+            ViewBag.GetImageUrl = response.ImageUrl;
+            ViewBag.GetSelf = response.Links.Self;
+            ViewBag.GetName = response.Name;
+            ViewBag.GetProperties = response.Properties;
+            ViewBag.GetTimestamp = response.Timestamp;
+
+            return View("index");
+        }
+
+        public IActionResult UpdateConversation(string id, string name, string displayName, string imageUrl)
+        {
+            var response = ConversationSender.UpdateConversation(id, name, displayName, imageUrl);
+            ViewBag.UpdateDisplayName = response.DisplayName;
+            ViewBag.UpdateId = response.Id;
+            ViewBag.UpdateImageUrl = response.ImageUrl;
+            ViewBag.UpdateSelf = response.Links.Self;
+            ViewBag.UpdateName = response.Name;
+            ViewBag.UpdateProperties = response.Properties;
+            ViewBag.UpdateTimestamp = response.Timestamp;
+            return View("index");
+        }
+
+        public IActionResult ListConversations()
+        {
+            var response = ConversationSender.ListConversation();
+            ViewBag.ConversationCount = response.Embedded.Conversations.Count;
+            return View("index");
+        }
+
+        public IActionResult DeleteConversation(string id)
+        {
+            var response = ConversationSender.DeleteConversation(id);
+            ViewBag.ConversationStatus = response == System.Net.HttpStatusCode.NoContent ? "Successfully Deleted" : "Something went wrong when deleting conversation " + response.ToString();
+            return View("index");
+        }
+    }
+}
---
+++ NexmoDotnetCodeSnippets/Senders/ConversationSender.cs
@@ -0,0 +1,113 @@
+﻿using Nexmo.Api;
+using NexmoDotnetCodeSnippets.Authentication;
+using Nexmo.Api.Conversations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace NexmoDotnetCodeSnippets.Senders
+{
+    public class ConversationSender
+    {
+        public class DemonstrativeProperties
+        {
+            public string foo { get; set; }
+        }
+
+        public static Nexmo.Api.Conversations.Conversation CreateConversation(string NAME, string DISPLAY_NAME, string IMAGE_URL)
+        {
+            var client = FullAuth.GetClient();
+            var PROPERTIES = new DemonstrativeProperties() { foo = "foo" };
+
+            var request = new CreateConversationRequest()
+            {
+                DisplayName = DISPLAY_NAME,
+                Name = NAME,
+                ImageUrl = IMAGE_URL,
+                Properties = new Properties(PROPERTIES)
+            };
+            var response = client.Conversation.CreateConversation(request);
+            return response;
+        }
+
+        public static Nexmo.Api.Conversations.Conversation GetConversation(string CONVERSATION_ID)
+        {
+            var client = FullAuth.GetClient();
+            var response = client.Conversation.GetConversation(CONVERSATION_ID);
+            return response;
+        }
+        
+        public static CursorBasedListResponse<ConversationList> ListConversation()
+        {
+            var client = FullAuth.GetClient();
+            uint PAGE_SIZE = 10;
+            string ORDER = "asc";
+            string CURSOR = "https://www.example.com/aconversation";
+            var parameters = new CursorListParams() { page_size = PAGE_SIZE, order = ORDER, cursor = CURSOR };
+            var response = client.Conversation.ListConversations(parameters);
+            return response;
+        }
+
+        public static Nexmo.Api.Conversations.Conversation UpdateConversation(string CONVERSATION_ID, string NAME, string DISPLAY_NAME, string IMAGE_URL)
+        {
+            var client = FullAuth.GetClient();
+            var PROPERTIES = new DemonstrativeProperties() { foo = "foo" };
+
+            var request = new UpdateConversationRequest() 
+            { 
+                DisplayName = DISPLAY_NAME, 
+                Name = NAME, 
+                ImageUrl = IMAGE_URL, 
+                Properties = new Properties(PROPERTIES)
+            };
+
+            var response = client.Conversation.UpdateConversation(request, CONVERSATION_ID);
+            return response;
+        }
+
+        public static HttpStatusCode DeleteConversation(string CONVERSATION_ID)
+        {
+            var client = FullAuth.GetClient();
+            var response = client.Conversation.DeleteConversation(CONVERSATION_ID);
+            return response;
+        }
+
+        public static User CreateUser(string NAME, string DISPLAY_NAME, string IMAGE_URL)
+        {
+            var client = FullAuth.GetClient();
+            var PROPERTIES = new DemonstrativeProperties() { foo = "foo" };
+            
+            var request = new CreateUserRequest()
+            {
+                DisplayName = DISPLAY_NAME,
+                Name = NAME,
+                ImageUrl = IMAGE_URL,
+                Properties = new Properties(PROPERTIES)
+            };
+
+            var response = client.Conversation.CreateUser(request);
+            return response;
+        }
+
+        public static User GetUser(string USER_ID)
+        {
+            var client = FullAuth.GetClient();
+
+            var response = client.Conversation.GetUser(USER_ID);
+            return response;
+        }
+
+        public static CursorBasedListResponse<UserList> ListUsers()
+        {
+            var client = FullAuth.GetClient();
+            uint PAGE_SIZE = 10;
+            string ORDER = "asc";
+            string CURSOR = "https://www.example.com/aUser";
+            var parameters = new CursorListParams() { page_size = PAGE_SIZE, order = ORDER, cursor = CURSOR };
+            var response = client.Conversation.ListUsers(parameters);
+            return response;
+        }
+    }
+}
