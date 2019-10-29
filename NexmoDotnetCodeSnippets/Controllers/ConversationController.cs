ac22ad933c24abce70a0788f805a51156f8d19ef
slorello89 <slorello89@gmail.com>
2019-10-29 15:37:27 -0400

    adding full snippet coverage for conversations

--- NexmoDotnetCodeSnippets/Controllers/ConversationController.cs
+++ NexmoDotnetCodeSnippets/Controllers/ConversationController.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
+using Nexmo.Api.Conversations;
 using NexmoDotnetCodeSnippets.Senders;
-using Microsoft.AspNetCore.Mvc;
 
 namespace NexmoDotnetCodeSnippets.Controllers
 {
@@ -14,13 +11,14 @@ namespace NexmoDotnetCodeSnippets.Controllers
             return View();
         }
 
+        [HttpPost]
         public IActionResult CreateConversation(string name, string displayName, string imageUrl)
         {
             var response = ConversationSender.CreateConversation(name, displayName, imageUrl);
             ViewBag.CreateDisplayName = response.DisplayName;
             ViewBag.CreateId = response.Id;
             ViewBag.CreateImageUrl = response.ImageUrl;
-            ViewBag.CreateSelf = response.Links.Self;
+            ViewBag.CreateSelf = response.Links.Self.Href;
             ViewBag.CreateName = response.Name;
             ViewBag.CreateProperties = response.Properties;
             ViewBag.CreateTimestamp = response.Timestamp;
@@ -28,13 +26,14 @@ namespace NexmoDotnetCodeSnippets.Controllers
             return View("index");
         }
 
+        [HttpPost]
         public IActionResult GetConversation(string id)
         {
             var response = ConversationSender.GetConversation(id);
             ViewBag.GetDisplayName = response.DisplayName;
             ViewBag.GetId = response.Id;
             ViewBag.GetImageUrl = response.ImageUrl;
-            ViewBag.GetSelf = response.Links.Self;
+            ViewBag.GetSelf = response.Links.Self.Href;
             ViewBag.GetName = response.Name;
             ViewBag.GetProperties = response.Properties;
             ViewBag.GetTimestamp = response.Timestamp;
@@ -42,19 +41,21 @@ namespace NexmoDotnetCodeSnippets.Controllers
             return View("index");
         }
 
+        [HttpPost]
         public IActionResult UpdateConversation(string id, string name, string displayName, string imageUrl)
         {
             var response = ConversationSender.UpdateConversation(id, name, displayName, imageUrl);
             ViewBag.UpdateDisplayName = response.DisplayName;
             ViewBag.UpdateId = response.Id;
             ViewBag.UpdateImageUrl = response.ImageUrl;
-            ViewBag.UpdateSelf = response.Links.Self;
+            ViewBag.UpdateSelf = response.Links.Self.Href;
             ViewBag.UpdateName = response.Name;
             ViewBag.UpdateProperties = response.Properties;
             ViewBag.UpdateTimestamp = response.Timestamp;
             return View("index");
         }
 
+        [HttpPost]
         public IActionResult ListConversations()
         {
             var response = ConversationSender.ListConversation();
@@ -62,10 +63,169 @@ namespace NexmoDotnetCodeSnippets.Controllers
             return View("index");
         }
 
+        [HttpPost]
         public IActionResult DeleteConversation(string id)
         {
             var response = ConversationSender.DeleteConversation(id);
             ViewBag.ConversationStatus = response == System.Net.HttpStatusCode.NoContent ? "Successfully Deleted" : "Something went wrong when deleting conversation " + response.ToString();
+            return View("index");
+        }
+
+        [HttpPost]
+        public IActionResult CreateUser(string name, string displayName, string imageUrl)
+        {
+            var response = ConversationSender.CreateUser(name, displayName, imageUrl);
+            ViewBag.UserCreateDisplayName = response.DisplayName;
+            ViewBag.UserCreateId = response.Id;
+            ViewBag.UserCreateImageUrl = response.ImageUrl;
+            ViewBag.UserCreateSelf = response.Links.Self.Href;
+            ViewBag.UserCreateName = response.Name;
+
+            return View("index");
+        }
+
+        [HttpPost]
+        public IActionResult GetUser(string id)
+        {
+            var response = ConversationSender.GetUser(id);
+            ViewBag.UserGetDisplayName = response.DisplayName;
+            ViewBag.UserGetId = response.Id;
+            ViewBag.UserGetImageUrl = response.ImageUrl;
+            ViewBag.UserGetSelf = response.Links.Self.Href;
+            ViewBag.UserGetName = response.Name;
+
+            return View("index");
+        }
+
+        [HttpPost]
+        public IActionResult ListUsers()
+        {
+            var response = ConversationSender.ListUsers();
+            ViewBag.UserCount = response.Embedded.Users.Count;
+            return View("index");
+        }
+
+        [HttpPost]
+        public IActionResult UpdateUser(string id, string name, string displayName, string imageUrl)
+        {
+            var response = ConversationSender.UpdateUserRequest(id, name, displayName, imageUrl);
+            ViewBag.UserUpdateDisplayName = response.DisplayName;
+            ViewBag.UserUpdateId = response.Id;
+            ViewBag.UserUpdateImageUrl = response.ImageUrl;
+            ViewBag.UserUpdateSelf = response.Links.Self.Href;
+            ViewBag.UserUpdateName = response.Name;
+            return View("index");
+        }
+
+        [HttpPost]
+        public IActionResult DeleteUser(string id)
+        {
+            var response = ConversationSender.DeleteUser(id);
+            ViewBag.UserStatus = response == System.Net.HttpStatusCode.NoContent ? "Successfully deleted user " : "Something went wrong while deleting Conversation " + response.ToString();
+            return View("index");
+        }
+
+        [HttpPost]
+        public IActionResult CreateMemberWithUserId(string conversationId, string userId, CreateMemberRequestBase.CreateMemberAction action)
+        {
+            var response = ConversationSender.CreateMemberWithUserId(conversationId, userId, action);
+            ViewBag.MemberCreateId = response.Id;
+            ViewBag.MemberCreatename = response.Name;
+            ViewBag.MemberCreateDisplayName = response.DisplayName;
+            return View("index");
+        }
+
+        [HttpPost]
+        public IActionResult CreateMemberWithUserName(string conversationId, string userName, CreateMemberRequestBase.CreateMemberAction action)
+        {
+            var response = ConversationSender.CreateMemberWithUserName(conversationId, userName, action);
+            ViewBag.MemberCreateWithNameid = response.Id;
+            ViewBag.MemberCreateWithNameName = response.Name;
+            ViewBag.MemberCreateWithNameDisplayName = response.DisplayName;
+            return View("index");
+        }
+
+        [HttpPost]
+        public IActionResult UpdateMember(string conversationId, string memberId, UpdateMemberRequest.MemberState state)
+        {
+            var response = ConversationSender.UpdateMember(conversationId, memberId, state);
+            ViewBag.MemberUpdateid = response.Id;
+            ViewBag.MemberUpdateName = response.Name;
+            ViewBag.MemberUpdateDisplayName = response.DisplayName;
+            ViewBag.MemberUpdateState = response.State;
+            return View("index");
+        }
+
+        [HttpPost]
+        public IActionResult GetMember(string conversationId, string memberId)
+        {
+            var response = ConversationSender.GetMember(conversationId, memberId);
+            ViewBag.MemberGetId = response.Id;
+            ViewBag.MemberGetName = response.Name;
+            ViewBag.MemberGetDisplayName = response.DisplayName;
+            ViewBag.MemberGetState = response.State;
+            return View("index");
+        }
+
+        [HttpPost]
+        public IActionResult ListMembers(string conversationId)
+        {
+            var response = ConversationSender.ListMembers(conversationId);
+
+            ViewBag.MemberCount = response.Embedded.Members.Count;
+            return View("index");
+        }
+
+        [HttpPost]
+        public IActionResult DeleteMember(string conversationId, string memberId)
+        {
+            var response = ConversationSender.DeleteMember(conversationId, memberId);
+
+            ViewBag.MemberStatus = response == System.Net.HttpStatusCode.NoContent ? "Successfully deleted member" : "Encountered an issue while deleteing member : " + response.ToString();
+            return View("index");
+        }
+
+        [HttpPost]
+        public IActionResult CreateTextEvent(string conversationId, string to, string from, string text)
+        {
+            var response = ConversationSender.CreateEventWithText(to, from, text, conversationId);
+
+            ViewBag.CreateTextEventId = response.Id;
+            return View("index");
+        }
+
+        [HttpPost]
+        public IActionResult CreateCustomEvent(string conversationid, string to, string from, string type)
+        {
+            var response = ConversationSender.CreateCustomEvent(type, to, from, conversationid);
+
+            ViewBag.CreateCustomEventId = response.Id;
+            return View("index");
+        }
+
+        [HttpPost]
+        public IActionResult ListEvents(string conversationId, string start_id, string end_id)
+        {
+            var response = ConversationSender.ListEvents(conversationId, start_id, end_id);
+
+            ViewBag.EventCount = response.Embedded.Events.Count;
+            return View("index");
+        }
+
+        [HttpPost]
+        public IActionResult GetEvent(string eventId, string conversationId)
+        {
+            var response = ConversationSender.GetEvent(conversationId, eventId);
+
+            ViewBag.GetEventId = response.Id;
+            return View("index");
+        }
+
+        [HttpPost]
+        public IActionResult DeleteEvent(string eventId, string conversationId)
+        {
+            var response = ConversationSender.DeleteEvent(conversationId, eventId);
+            ViewBag.EventStatus = response == System.Net.HttpStatusCode.NoContent ? "Event deleted successfully" : "Encountered a problem when deleting event " + response.ToString();
             return View("index");
         }
     }
--- NexmoDotnetCodeSnippets/Senders/ConversationSender.cs
+++ NexmoDotnetCodeSnippets/Senders/ConversationSender.cs
@@ -16,6 +16,11 @@ namespace NexmoDotnetCodeSnippets.Senders
             public string foo { get; set; }
         }
 
+        public class DemonstrativeCustomEventBody
+        {
+            public string foo { get; set; }
+        }
+
         public static Nexmo.Api.Conversations.Conversation CreateConversation(string NAME, string DISPLAY_NAME, string IMAGE_URL)
         {
             var client = FullAuth.GetClient();
@@ -107,6 +112,167 @@ namespace NexmoDotnetCodeSnippets.Senders
             string CURSOR = "https://www.example.com/aUser";
             var parameters = new CursorListParams() { page_size = PAGE_SIZE, order = ORDER, cursor = CURSOR };
             var response = client.Conversation.ListUsers(parameters);
+            return response;
+        }
+
+        public static User UpdateUserRequest(string USER_ID, string NAME, string DISPLAY_NAME, string IMAGE_URL)
+        {
+            var client = FullAuth.GetClient();
+
+            var PROPERTIES = new DemonstrativeProperties() { foo = "bar" };
+
+            var request = new UpdateUserRequest()
+            {
+                Name = NAME,
+                DisplayName = DISPLAY_NAME,
+                ImageUrl = IMAGE_URL,
+                Properties = new Properties(PROPERTIES)
+            };
+
+            var response = client.Conversation.UpdateUser(request,USER_ID);
+
+            return response;
+        }
+
+        public static HttpStatusCode DeleteUser(string USER_ID)
+        {
+            var client = FullAuth.GetClient();
+
+            var response = client.Conversation.DeleteUser(USER_ID);
+
+            return response;
+        }
+
+        public static Member CreateMemberWithUserId(string CONVERSATION_ID, string USER_ID, CreateMemberRequestBase.CreateMemberAction ACTION)
+        {
+            var client = FullAuth.GetClient();
+
+            var request = new CreateMemberWithIdRequest()
+            {
+                Id = USER_ID,
+                Action = ACTION
+            };
+
+            var response = client.Conversation.CreateMember(request, CONVERSATION_ID);
+
+            return response;
+        }
+
+        public static Member CreateMemberWithUserName(string CONVERSATION_ID, string USER_NAME, CreateMemberRequestBase.CreateMemberAction ACTION)
+        {
+            var client = FullAuth.GetClient();
+
+            var request = new CreateMemberWithNameRequest()
+            {
+                Name = USER_NAME,
+                Action = ACTION
+            };
+
+            var response = client.Conversation.CreateMember(request, CONVERSATION_ID);
+            return response;
+        }
+
+        public static Member GetMember(string CONVERSATION_ID, string MEMBER_ID)
+        {
+            var client = FullAuth.GetClient();
+            var response = client.Conversation.GetMember(MEMBER_ID, CONVERSATION_ID);
+            return response;
+        }
+
+        public static CursorBasedListResponse<MemberList> ListMembers(string CONVERSATION_ID)
+        {
+            var client = FullAuth.GetClient();
+            uint PAGE_SIZE = 10;
+            string ORDER = "asc";
+            string CURSOR = "https://www.example.com/aUser";
+
+            var parameters = new CursorListParams() { page_size = PAGE_SIZE, order = ORDER, cursor = CURSOR };
+            var response = client.Conversation.ListMembers(parameters, CONVERSATION_ID);
+
+            return response;
+        }
+
+        public static Member UpdateMember(string CONVERSATION_ID, string MEMBER_ID, UpdateMemberRequest.MemberState STATE)
+        {
+            var client = FullAuth.GetClient();
+
+            var request = new UpdateMemberRequest() { State = STATE };
+
+            var response = client.Conversation.UpdateMember(request, MEMBER_ID, CONVERSATION_ID);
+
+            return response;
+        }
+
+        public static HttpStatusCode DeleteMember(string CONVERSATION_ID, string MEMBER_ID)
+        {
+            var client = FullAuth.GetClient();
+            var response = client.Conversation.DeleteMember(MEMBER_ID, CONVERSATION_ID);
+            return response;
+        }
+
+        public static Event CreateEventWithText(string TO, string FROM, string TEXT, string CONVERSATION_ID)
+        {
+            var client = FullAuth.GetClient();
+
+            var request = new CreateTextEventRequest()
+            {
+                Body = new TextEventBody()
+                {
+                    Text = TEXT
+                },
+                To = TO,
+                From = FROM
+            };
+
+            var response = client.Conversation.CreateEvent(request, CONVERSATION_ID);
+
+            return response;
+        }
+
+        public static Event CreateCustomEvent(string TYPE, string TO, string FROM, string CONVERSATION_ID)
+        {
+            var client = FullAuth.GetClient();
+            var BODY = new DemonstrativeCustomEventBody() { foo = "bar" };
+            var request = new CreateCustomEventRequest<DemonstrativeCustomEventBody>()
+            {
+                Body = BODY,
+                From = FROM,
+                To = TO,
+                Type = TYPE
+            };
+            var response = client.Conversation.CreateEvent(request, CONVERSATION_ID);
+            return response;
+        }
+
+        public static CursorBasedListResponse<EventList> ListEvents(string CONVERSATION_ID, string START_ID, string END_ID)
+        {
+            var client = FullAuth.GetClient();
+
+            var parameters = new EventListParams()
+            {
+                start_id = START_ID,
+                end_id = END_ID
+            };
+            var response = client.Conversation.ListEvents(parameters, CONVERSATION_ID);
+
+            return response;
+        }
+
+        public static Event GetEvent(string CONVERSATION_ID, string EVENT_ID)
+        {
+            var client = FullAuth.GetClient();
+
+            var response = client.Conversation.GetEvent(EVENT_ID, CONVERSATION_ID);
+
+            return response;
+        }
+
+        public static HttpStatusCode DeleteEvent(string CONVERSATION_ID, string EVENT_ID)
+        {
+            var client = FullAuth.GetClient();
+
+            var response = client.Conversation.DeleteEvent(EVENT_ID, CONVERSATION_ID);
+
             return response;
         }
     }
