220526ac654399fa8bd8078ad32ca53b8b01e484
slorello89 <slorello89@gmail.com>
2020-06-02 17:09:14 -0400

    Adding ASR snippets

---
+++ NexmoDotnetCodeSnippets/Controllers/AsrController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Nexmo.Api;
+using Nexmo.Api.Voice.AnswerWebhooks;
+using Nexmo.Api.Voice.EventWebhooks;
+using Nexmo.Api.Voice.Nccos;
+
+namespace NexmoDotnetCodeSnippets.Controllers
+{
+    public class AsrController : Controller
+    {
+        [HttpGet("/webhooks/answer")]
+        public string Answer([FromQuery]Answer request)
+        {
+            var speechSettings = new SpeechSettings {Language="en-US", EndOnSilence=1, Uuid = new[] { request.Uuid } };
+            var inputAction = new MultiInputAction { Speech = speechSettings, EventUrl = new[] { "http://ngrok.io.slorello.ngrok.io/webhooks/asr" } };
+
+            var talkAction = new TalkAction { Text = "Please speak now" };
+
+            var ncco = new Ncco(talkAction, inputAction);
+            var ret = ncco.ToString();
+            return ret;
+        }
+
+        [HttpPost("/webhooks/asr")]
+        public string OnInput()
+        {
+            MultiInput input;
+            using (StreamReader reader = new StreamReader(Request.Body, Encoding.UTF8))
+            {
+                var result = reader.ReadToEndAsync().Result;
+                input = JsonConvert.DeserializeObject<MultiInput>(result);
+            }
+            var talkAction = new TalkAction();
+            talkAction.Text = input.Speech.SpeechResults[0].Text;
+            var ncco = new Ncco(talkAction);
+            return ncco.ToString();
+        }
+    }
+}
