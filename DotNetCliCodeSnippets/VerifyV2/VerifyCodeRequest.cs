a42c83f4a964804ac9d1ee9749096ee569c4a6fc
tr00d <guillaume.faas@live.fr>
2023-05-22 16:07:29 +0200

    Update Vonage package to v6.3.2

--- DotNetCliCodeSnippets/VerifyV2/VerifyCodeRequest.cs
+++ DotNetCliCodeSnippets/VerifyV2/VerifyCodeRequest.cs
@@ -2,27 +2,29 @@ using System;
 using System.Threading.Tasks;
 using Vonage;
 using Vonage.Request;
-using Vonage.VerifyV2;
 
 namespace DotnetCliCodeSnippets.VerifyV2;
 
-public class CancelRequest : ICodeSnippet
+public class VerifyCodeRequest : ICodeSnippet
 {
     public async Task Execute()
     {
         var appId = Environment.GetEnvironmentVariable("VONAGE_APP_ID") ?? "VONAGE_APP_ID";
         var privateKeyPath = Environment.GetEnvironmentVariable("VONAGE_PRIVATE_KEY_PATH") ?? "VONAGE_PRIVATE_KEY_PATH";
         var requestId = Guid.Parse(Environment.GetEnvironmentVariable("REQUEST_ID") ?? "REQUEST_ID");            
-
+            
         var credentials = Credentials.FromAppIdAndPrivateKeyPath(appId, privateKeyPath);
         var client = new VonageClient(credentials);
-
-        var request = Vonage.VerifyV2.Cancel.CancelRequest.Parse(requestId);
-
-        var response = await client.VerifyV2Client.CancelAsync(request);
-        var message = response.Match(
-            success => "Request has been cancelled", 
-            failure => $"Request couldn't be cancelled: {failure.GetFailureMessage()}");
-        Console.WriteLine(message);
+            
+        // var request = VerifyCodeRequest.Build()
+        //     .WithRequestId(requestId)
+        //     .WithCode("123456")
+        //     .Create();
+        //         
+        // var response = await client.VerifyV2Client.VerifyCodeAsync(request);
+        // var message = response.Match(
+        //     success => "Request verification succeeded.", 
+        //     failure => $"Request verification failed: {failure.GetFailureMessage()}");
+        // Console.WriteLine(message);
     }
 }
