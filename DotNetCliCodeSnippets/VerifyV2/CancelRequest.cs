4ea77a91c9f9428993efc4f2e51dc11fc3e0026a
Tr00d <guillaume.faas@live.fr>
2023-05-26 07:12:04 +0200

    Add snippets for VerifyV2

--- DotNetCliCodeSnippets/VerifyV2/CancelRequest.cs
+++ DotNetCliCodeSnippets/VerifyV2/CancelRequest.cs
@@ -2,7 +2,6 @@ using System;
 using System.Threading.Tasks;
 using Vonage;
 using Vonage.Request;
-using Vonage.VerifyV2;
 
 namespace DotnetCliCodeSnippets.VerifyV2;
 
@@ -12,16 +11,13 @@ public class CancelRequest : ICodeSnippet
     {
         var appId = Environment.GetEnvironmentVariable("VONAGE_APP_ID") ?? "VONAGE_APP_ID";
         var privateKeyPath = Environment.GetEnvironmentVariable("VONAGE_PRIVATE_KEY_PATH") ?? "VONAGE_PRIVATE_KEY_PATH";
-        var requestId = Guid.Parse(Environment.GetEnvironmentVariable("REQUEST_ID") ?? "REQUEST_ID");            
-
+        var requestId = Guid.Parse(Environment.GetEnvironmentVariable("REQUEST_ID") ?? "REQUEST_ID");
         var credentials = Credentials.FromAppIdAndPrivateKeyPath(appId, privateKeyPath);
         var client = new VonageClient(credentials);
-
         var request = Vonage.VerifyV2.Cancel.CancelRequest.Parse(requestId);
-
         var response = await client.VerifyV2Client.CancelAsync(request);
         var message = response.Match(
-            success => "Request has been cancelled", 
+            success => "Request has been cancelled",
             failure => $"Request couldn't be cancelled: {failure.GetFailureMessage()}");
         Console.WriteLine(message);
     }
--- DotNetCliCodeSnippets/VerifyV2/VerifyCodeRequest.cs
+++ DotNetCliCodeSnippets/VerifyV2/VerifyCodeRequest.cs
@@ -11,20 +11,18 @@ public class VerifyCodeRequest : ICodeSnippet
     {
         var appId = Environment.GetEnvironmentVariable("VONAGE_APP_ID") ?? "VONAGE_APP_ID";
         var privateKeyPath = Environment.GetEnvironmentVariable("VONAGE_PRIVATE_KEY_PATH") ?? "VONAGE_PRIVATE_KEY_PATH";
-        var requestId = Guid.Parse(Environment.GetEnvironmentVariable("REQUEST_ID") ?? "REQUEST_ID");            
-            
+        var requestId = Guid.Parse(Environment.GetEnvironmentVariable("REQUEST_ID") ?? "REQUEST_ID");
+        var verificationCode = Environment.GetEnvironmentVariable("VERIFICATION_CODE") ?? "VERIFICATION_CODE";
         var credentials = Credentials.FromAppIdAndPrivateKeyPath(appId, privateKeyPath);
         var client = new VonageClient(credentials);
-            
-        // var request = VerifyCodeRequest.Build()
-        //     .WithRequestId(requestId)
-        //     .WithCode("123456")
-        //     .Create();
-        //         
-        // var response = await client.VerifyV2Client.VerifyCodeAsync(request);
-        // var message = response.Match(
-        //     success => "Request verification succeeded.", 
-        //     failure => $"Request verification failed: {failure.GetFailureMessage()}");
-        // Console.WriteLine(message);
+        var request = Vonage.VerifyV2.VerifyCode.VerifyCodeRequest.Build()
+            .WithRequestId(requestId)
+            .WithCode(verificationCode)
+            .Create();
+        var response = await client.VerifyV2Client.VerifyCodeAsync(request);
+        var message = response.Match(
+            success => "Code verification succeeded.",
+            failure => $"Code verification failed: {failure.GetFailureMessage()}");
+        Console.WriteLine(message);
     }
 }
