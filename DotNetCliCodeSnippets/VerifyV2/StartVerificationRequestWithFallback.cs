091911848d7c2244747bbd6518ab8872aac67ac1
tr00d <guillaume.faas@live.fr>
2023-05-31 15:04:27 +0200

    Match fallback workflow with snippet

--- DotNetCliCodeSnippets/VerifyV2/StartVerificationRequestWithFallback.cs
+++ DotNetCliCodeSnippets/VerifyV2/StartVerificationRequestWithFallback.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Vonage;
 using Vonage.Request;
 using Vonage.VerifyV2.StartVerification;
+using Vonage.VerifyV2.StartVerification.Email;
 using Vonage.VerifyV2.StartVerification.SilentAuth;
 using Vonage.VerifyV2.StartVerification.Sms;
 using Vonage.VerifyV2.StartVerification.WhatsApp;
@@ -14,6 +15,7 @@ public class StartVerificationRequestWithFallback: ICodeSnippet
     public async Task Execute()
     {
         var TO_NUMBER = Environment.GetEnvironmentVariable("TO_NUMBER") ?? "TO_NUMBER";
+        var TO_EMAIL = Environment.GetEnvironmentVariable("TO_EMAIL") ?? "TO_EMAIL";
         var BRAND_NAME = Environment.GetEnvironmentVariable("VONAGE_BRAND_NAME") ?? "VONAGE_BRAND_NAME";
         var VONAGE_APPLICATION_ID = Environment.GetEnvironmentVariable("VONAGE_APP_ID") ?? "VONAGE_APP_ID";
         var VONAGE_APPLICATION_PRIVATE_KEY_PATH = Environment.GetEnvironmentVariable("VONAGE_PRIVATE_KEY_PATH") ?? "VONAGE_PRIVATE_KEY_PATH";
@@ -21,9 +23,8 @@ public class StartVerificationRequestWithFallback: ICodeSnippet
         var client = new VonageClient(credentials);
         var request = StartVerificationRequest.Build()
             .WithBrand(BRAND_NAME)
-            .WithWorkflow(SmsWorkflow.Parse(TO_NUMBER))
-            .WithFallbackWorkflow(WhatsAppWorkflow.Parse(TO_NUMBER))
-            .WithFallbackWorkflow(SilentAuthWorkflow.Parse(TO_NUMBER))
+            .WithWorkflow(SilentAuthWorkflow.Parse(TO_NUMBER))
+            .WithFallbackWorkflow(EmailWorkflow.Parse(TO_EMAIL))
             .Create();
         var response = await client.VerifyV2Client.StartVerificationAsync(request);
         var message = response.Match(
