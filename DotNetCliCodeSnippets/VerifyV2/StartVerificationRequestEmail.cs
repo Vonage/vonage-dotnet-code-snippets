fa0b66eacd5b7c9694ac9cc7df561b8306d1f591
tr00d <guillaume.faas@live.fr>
2023-05-31 14:57:10 +0200

    Change variable names on VerifyV2 to match portal snippet

--- DotNetCliCodeSnippets/VerifyV2/StartVerificationRequestEmail.cs
+++ DotNetCliCodeSnippets/VerifyV2/StartVerificationRequestEmail.cs
@@ -11,15 +11,15 @@ public class StartVerificationRequestEmail : ICodeSnippet
 {
     public async Task Execute()
     {
-        var to = Environment.GetEnvironmentVariable("TO_EMAIL") ?? "TO_EMAIL";
-        var brandName = Environment.GetEnvironmentVariable("VONAGE_BRAND_NAME") ?? "VONAGE_BRAND_NAME";
-        var appId = Environment.GetEnvironmentVariable("VONAGE_APP_ID") ?? "VONAGE_APP_ID";
-        var privateKeyPath = Environment.GetEnvironmentVariable("VONAGE_PRIVATE_KEY_PATH") ?? "VONAGE_PRIVATE_KEY_PATH";
-        var credentials = Credentials.FromAppIdAndPrivateKeyPath(appId, privateKeyPath);
+        var TO_EMAIL = Environment.GetEnvironmentVariable("TO_EMAIL") ?? "TO_EMAIL";
+        var BRAND_NAME = Environment.GetEnvironmentVariable("VONAGE_BRAND_NAME") ?? "VONAGE_BRAND_NAME";
+        var VONAGE_APPLICATION_ID = Environment.GetEnvironmentVariable("VONAGE_APP_ID") ?? "VONAGE_APP_ID";
+        var VONAGE_APPLICATION_PRIVATE_KEY_PATH = Environment.GetEnvironmentVariable("VONAGE_PRIVATE_KEY_PATH") ?? "VONAGE_PRIVATE_KEY_PATH";
+        var credentials = Credentials.FromAppIdAndPrivateKeyPath(VONAGE_APPLICATION_ID, VONAGE_APPLICATION_PRIVATE_KEY_PATH);
         var client = new VonageClient(credentials);
         var request = StartVerificationRequest.Build()
-            .WithBrand(brandName)
-            .WithWorkflow(EmailWorkflow.Parse(to))
+            .WithBrand(BRAND_NAME)
+            .WithWorkflow(EmailWorkflow.Parse(TO_EMAIL))
             .Create();
         var response = await client.VerifyV2Client.StartVerificationAsync(request);
         var message = response.Match(
