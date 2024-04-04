fdfb8fdf0d631721c3b6896ed498453481675421
superchilled <karl@superchilled.co.uk>
2024-04-04 12:11:40 +0100

    Adding appsettings comments

--- DotNetCliCodeSnippets/Messages/Sandbox/WhatsApp/SendWhatsAppText.cs
+++ DotNetCliCodeSnippets/Messages/Sandbox/WhatsApp/SendWhatsAppText.cs
@@ -15,6 +15,7 @@ public class SendWhatsAppText : ICodeSnippet
         var appId = Environment.GetEnvironmentVariable("VONAGE_APP_ID") ?? "VONAGE_APP_ID";
         var privateKeyPath = Environment.GetEnvironmentVariable("VONAGE_PRIVATE_KEY_PATH") ?? "VONAGE_PRIVATE_KEY_PATH";
 
+        // Set "Url.Api" to "https://messages-sandbox.nexmo.com" in appsettings.json
         var credentials = Credentials.FromAppIdAndPrivateKeyPath(appId, privateKeyPath);
 
         var vonageClient = new VonageClient(credentials);
