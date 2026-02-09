55778613051b7257a99520c0f1f8e96804f29547
tr00d <guillaume.faas@live.fr>
2026-02-09 07:06:33 +0100

    feat: add snippet for status indicator in WhatsApp messages

---
+++ DotNetCliCodeSnippets/Messages/WhatsApp/MarkAsReadWithStatusIndicator.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Threading.Tasks;
+using Vonage;
+using Vonage.Messages.WhatsApp;
+using Vonage.Request;
+
+namespace DotnetCliCodeSnippets.Messages.WhatsApp;
+
+public class MarkAsReadWithStatusIndicator : ICodeSnippet
+{
+    public async Task Execute()
+    {
+        var MESSAGES_MESSAGE_ID = Environment.GetEnvironmentVariable("MESSAGES_MESSAGE_ID") ?? "MESSAGES_MESSAGE_ID";
+        var VONAGE_APPLICATION_ID = Environment.GetEnvironmentVariable("VONAGE_APPLICATION_ID") ?? "VONAGE_APPLICATION_ID";
+        var VONAGE_PRIVATE_KEY_PATH = Environment.GetEnvironmentVariable("VONAGE_PRIVATE_KEY_PATH") ?? "VONAGE_PRIVATE_KEY_PATH";
+        var credentials = Credentials.FromAppIdAndPrivateKeyPath(VONAGE_APPLICATION_ID, VONAGE_PRIVATE_KEY_PATH);
+        var vonageClient = new VonageClient(credentials);
+        await vonageClient.MessagesClient.UpdateAsync(WhatsAppUpdateMessageRequest.Build(MESSAGES_MESSAGE_ID, new WhatsAppReplyingIndicator(true)));
+    }
+}
